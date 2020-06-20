ae32e9aa2bdc690ed76e2e02802f9636a4a974b9
Ernest Ortuño <ernest.ort.tor@gmail.com>
2020-06-20 20:36:08 +0200

    m

---
+++ dashboard.js
@@ -0,0 +1,67 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          1
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      },
+      {
+              data: [
+                720
+              ],
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#007bff',
+              borderWidth: 4,
+              pointBackgroundColor: '#007bff'
+            },
+            {
+                          data: [
+                            2.4
+                          ],
+                          lineTension: 0,
+                          backgroundColor: 'transparent',
+                          borderColor: '#007bff',
+                          borderWidth: 4,
+                          pointBackgroundColor: '#007bff'
+                        }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+}())
